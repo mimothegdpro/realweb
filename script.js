d409e6ee6730730b6896785013799cf5fd7ad8a2
mimothegdpro <mimothegdpro@gmail.com>
2026-01-02 06:29:27 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+let box = document.getElementById("box");
+
+let velocity = 0;
+let gravity = -0.5;
+let isJumping = false;
+let position = 0;
+
+function jump() {
+  if (!isJumping) {
+    velocity = 10;     // jump strength
+    isJumping = true;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.code === "Space") {
+    jump();
+  }
+});
+
+function gameLoop() {
+  velocity += gravity;
+  position += velocity;
+
+  if (position <= 0) {
+    position = 0;
+    velocity = 0;
+    isJumping = false;
+  }
+
+  box.style.bottom = position + "px";
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
 
